3ae80a1562ea3319ad762c1ad8b13863e32b38f7
FHSWar <fhswar0504@163.com>
2021-10-09 13:18:45 +0800

    淦！能缓存的用 onUpdated，不能缓存的用 onMounted 啊！很容易

--- src/views/home/Bill.vue
+++ src/views/home/Bill.vue
@@ -1,11 +1,17 @@
 <script setup>
 import BillUpper from '../../components/home/BillUpper.vue'
 import BillMain from '../../components/home/BillMain.vue'
+
+import { onMounted } from 'vue'
+onMounted(()=>{
+	console.log('这不得试试？')
+})
+
 </script>
 
 <template>
 	<div class="flex flex-col relative">
-		<BillUpper/>
+		<BillUpper />
 		<BillMain />
 	</div>
 </template>
